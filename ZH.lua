9ffd604c80e2fc1d128016457dc7656c496d3c28
Mikeeee <gegidzezviad05@gmail.com>
2025-12-25 04:32:24 +0400

    Update ZH.lua

--- ZH.lua
+++ ZH.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/485a4770312d61515072c8353a678992dccba3522bb323c0403bf1fff25b5a45/download"))()
+loadstring(game:HttpGet("https://zh-ontop.vercel.app/api/loader"))()
